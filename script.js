8ec560a846d4afac3a0fef342feac99cddab7286
cody <165951965+theinfamouscoder5@users.noreply.github.com>
2025-02-21 14:12:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+const greetingElement = document.getElementById('greeting');
 
-
+function getGreeting() {
+    const currentTime = new Date().getHours();
+    let greeting;
+
+    if (currentTime < 12) {
+        greeting = 'Good morning';
+    } else if (currentTime < 18) {
+        greeting = 'Good afternoon';
+    } else {
+        greeting = 'Good evening';
+    }
+
+    greetingElement.textContent = greeting;
+}
+
+getGreeting();
